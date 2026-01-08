c2c7a8ea0e9f4ce4bf9c4d78e56baf69593323b6
Muass Repal <ardiyantorepal@gmail.com>
2026-01-08 20:40:56 +0700

    Create AuthController.php

---
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Admin;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+        $admin = Admin::where('username', $request->username)->first();
+
+        if (!$admin || !Hash::check($request->password, $admin->password)) {
+            return response()->json(['message' => 'Login gagal'], 401);
+        }
+
+        return response()->json([
+            'message' => 'Login berhasil',
+            'admin' => $admin
+        ]);
+    }
+}
